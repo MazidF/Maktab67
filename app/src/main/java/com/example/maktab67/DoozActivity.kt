66644b00da6d6cd8dfbb9b1cd5cacdfcc32947e7
m.m <m3farahani1382@gmail.com>
2022-01-17 00:40:24 +0330

    Tic Tac Toe with bot :)

--- app/src/main/java/com/example/maktab67/DoozActivity.kt
+++ app/src/main/java/com/example/maktab67/DoozActivity.kt
@@ -1,14 +1,17 @@
 package com.example.maktab67
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.example.maktab67.databinding.ActivityDoozBinding
 
 class DoozActivity : AppCompatActivity() {
@@ -18,11 +21,29 @@ class DoozActivity : AppCompatActivity() {
         const val RED_TURN = "Red Turn"
         const val BLUE_TURN = "Blue Turn"
         const val DRAW = "Draw"
+        val bestPlaces = arrayOf(4, 0, 2, 6, 8, 1, 3, 5, 7)
     }
 
+    var hasBot = false
+    lateinit var botTarget: HashSet<Int>
+    lateinit var botThreat: HashSet<Int>
+    var botValue = 2
+    set(value) {
+/*        if (value == 1) { // bot -> blue
+            currentIsBlue = false
+            isBlueTurn = true
+        } else if (value == -1) { // bot -> red
+            currentIsBlue = true
+            isBlueTurn = false
+        }*/
+        field = value
+    } get() {
+        if (field == 2) throw Exception("bot value has a problem!!")
+        return field
+    }
     var remainPlaces = 9
-    var redPoints = -1
-    var bluePoints = -1
+    var redPoints = 0
+    var bluePoints = 0
     var isPlaying = true
     var isBlueTurn = true
     var currentIsBlue = true
@@ -35,16 +56,30 @@ class DoozActivity : AppCompatActivity() {
     lateinit var turnView: TextView
     lateinit var binding: ActivityDoozBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDoozBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+        hasBot = intent.getBooleanExtra("hasBot", false)
         load()
         init()
     }
 
-    private fun log(msg: String, tag: String = "tag") {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        if (!hasBot) {
+            menu.add("Bot").setOnMenuItemClickListener {
+                val intent = Intent(this, DoozActivity::class.java).apply {
+                    putExtra("hasBot", true)
+                }
+                startActivity(intent)
+                false
+            }
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun log(msg: String, tag: String = "myLog") {
         Log.e(tag, msg)
     }
 
@@ -54,7 +89,7 @@ class DoozActivity : AppCompatActivity() {
     }
 
     private fun save() {
-        log("save method")
+        if (hasBot) return
         val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
         sharedPreferences.edit()
             .putBoolean("isBlueTurn", isBlueTurn)
@@ -69,11 +104,12 @@ class DoozActivity : AppCompatActivity() {
 
     @SuppressLint("WrongConstant")
     private fun load() {
+        if (hasBot) return
         val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
         with(sharedPreferences) {
             redPoints = getInt("redPoints", 0)
             bluePoints = getInt("bluePoints", 0)
-            remainPlaces = getInt("remainPlaces", 0)
+            remainPlaces = getInt("remainPlaces", 9)
             isBlueTurn = getBoolean("isBlueTurn", true)
             currentIsBlue = getBoolean("currentIsBlue", true)
             val string = getString("icons", null)
@@ -85,6 +121,23 @@ class DoozActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        if (hasBot) {
+            botTarget = HashSet(9)
+            botThreat = HashSet(9)
+            val dialog = AlertDialog.Builder(this)
+                .setCancelable(false)
+                .setMessage("Which one starts the game?")
+                .setNeutralButton("Return") { _, _ ->
+                    finish()
+                }
+                .setPositiveButton("Me") { _, _ ->
+                    botValue = -1
+                }.setNegativeButton("Bot") {_, _ ->
+                    botValue = 1
+                    decision()
+                }.create()
+            dialog.show()
+        }
         turnView = binding.turnViewer
         setTurn()
         with(binding) {
@@ -120,21 +173,59 @@ class DoozActivity : AppCompatActivity() {
     }
 
     fun onImageClick(view: View) {
+        onImageClick(view, false)
+    }
+
+    private fun onImageClick(view: View, fromBot: Boolean) {
         val image = view as ImageView
         if (!isPlaying || view.drawable != null) return
+        if (hasBot && ((currentIsBlue && botValue == 1)  || (!currentIsBlue && botValue == -1))) {
+            if (!fromBot) return
+        }
 
         remainPlaces--
         turnView.alpha = 0f
         view.translationY = -2000f
 
-        image.setImageResource(getImage(view.tag as Int))
-        currentIsBlue = !currentIsBlue
-        setTurn()
+        val index = view.tag as Int
+        runOnUiThread{
+            image.setImageResource(getImage(index))
+            currentIsBlue = !currentIsBlue
+            setTurn()
+        }
 
         view.animate().translationY(0f).duration = 400
         turnView.animate().alpha(1f).duration = 800
 
-        check()
+
+        if (hasBot) {
+            botCheck(index, fromBot)
+            if (fromBot) {
+                check()
+            } else {
+                if (!check()) {
+                    Thread {
+                        Thread.sleep(2000)
+                        decision()
+                    }.start()
+                    log("continue after")
+                }
+            }
+        } else {
+            check()
+        }
+
+/*        if (fromBot) {
+            check()
+        } else {
+            if (!check()) {
+                Thread {
+                    Thread.sleep(2000)
+                    decision()
+                }.start()
+                log("continue after")
+            }
+        }*/
     }
 
     private fun draw() {
@@ -146,7 +237,7 @@ class DoozActivity : AppCompatActivity() {
         isPlaying = false
     }
 
-    private fun check(addPoint: Boolean = true) {
+    private fun check(addPoint: Boolean = true): Boolean {
         var value = 0
 
         fun equals(a: Int, b: Int, c: Int) {
@@ -167,9 +258,58 @@ class DoozActivity : AppCompatActivity() {
         equals(0, 4, 8)
         equals(2, 4, 6)
         if (value != 0) {
-            win(addPoint)
+            runOnUiThread {
+                win(addPoint)
+            }
+            return true
         } else if (remainPlaces == 0) {
-            draw()
+            runOnUiThread {
+                draw()
+            }
+            return true
+        }
+        return false
+    }
+
+    // this method invoked if game is not over !!!
+    private fun botCheck(lastMove: Int, fromBot: Boolean) {
+        if (!hasBot) return
+        val value = if (fromBot) botValue else -botValue
+        icons[lastMove] = value
+        botTarget.remove(lastMove)
+        botThreat.remove(lastMove)
+        fun checker(a: Int, c: Int) {
+            if (icons[a] == value && icons[c] == 0) {
+                if (fromBot) {
+                    botTarget.add(c)
+                } else {
+                    botThreat.add(c)
+                }
+                return
+            }
+            if (icons[c] == value && icons[a] == 0) {
+                if (fromBot) {
+                    botTarget.add(a)
+                } else {
+                    botThreat.add(a)
+                }
+            }
+        }
+
+        val reminder = (lastMove / 3) * 3
+        checker((lastMove + 1) % 3 + reminder, (lastMove + 2) % 3 + reminder)
+        checker((lastMove + 3) % 9, (lastMove + 6) % 9)
+        when {
+            lastMove % 8 == 0 -> {
+                checker(4, if (0 == lastMove) 8 else 0)
+            }
+            lastMove % 4 == 2 -> {
+                checker(4, if (2 == lastMove) 6 else 2)
+            }
+            lastMove == 4 -> {
+                checker(0, 8)
+                checker(2, 6)
+            }
         }
     }
 
@@ -208,13 +348,20 @@ class DoozActivity : AppCompatActivity() {
         currentIsBlue = isBlueTurn
         setTurn()
         binding.doozRestartBtn.visibility = View.INVISIBLE
+        if (hasBot) { // when bot should start the game
+            if (isBlueTurn) {
+                if (botValue == 1) decision()
+            } else {
+                if (botValue == -1) decision()
+            }
+        }
     }
 
     private fun getImage(tag: Int): Int {
        return if (currentIsBlue) {
            icons[tag] = 1
            R.drawable.blue_o_icon
-        } else {
+       } else {
            icons[tag] = -1
            R.drawable.red_x_icon
        }
@@ -222,5 +369,29 @@ class DoozActivity : AppCompatActivity() {
 
     fun reset() {
 
+    }
+
+    private fun decision() {
+        if (!hasBot) return
+        var index: Int = -1
+        if (botTarget.isNotEmpty()) {
+            index = botTarget.stream().toArray()[0] as Int
+            botTarget.remove(index)
+        } else if (botThreat.isNotEmpty()) {
+            index = botThreat.stream().toArray()[0] as Int
+            botThreat.remove(index)
+        } else {
+            for (i in bestPlaces) {
+                if (icons[i] == 0) {
+                    index = i
+                    break
+                }
+            }
+        }
+        if (index == -1) {
+            throw Exception("either remaining palaces is 0 or something else!!")
+        }
+        val view = views[index]
+        onImageClick(view, true)
     }
 }
