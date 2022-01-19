e62c9d69e7f7fd3985623bd1baa06c812b5662d0
m.m <m3farahani1382@gmail.com>
2022-01-19 22:15:51 +0330

    morabaraba board is ready

--- app/src/main/java/com/example/maktab67/MorabarabaActivity.kt
+++ app/src/main/java/com/example/maktab67/MorabarabaActivity.kt
@@ -1,22 +1,12 @@
 package com.example.maktab67
 
 import android.annotation.SuppressLint
-import android.content.res.ColorStateList
-import android.graphics.Color.RED
-import android.graphics.PorterDuff
-import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.example.maktab67.databinding.ActivityMorabarabaBinding
 import android.util.DisplayMetrics
-import android.view.View
-import android.widget.FrameLayout
 import android.widget.ImageView
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.content.ContextCompat
-import androidx.core.view.allViews
-import androidx.core.widget.ImageViewCompat
 
 
 class MorabarabaActivity : AppCompatActivity() {
@@ -38,15 +28,8 @@ class MorabarabaActivity : AppCompatActivity() {
         getSize()
         board = Array(3) { Array(9) { 0 } }
         with(binding) {
-            this@MorabarabaActivity.views = arrayOf(list1.init(), list2.init(), list3.init())
-            root.addView(View.inflate(this@MorabarabaActivity, R.layout.mylayout, null))
+            list3.view1.setImageResource(R.drawable.dice_1)
         }
-    }
-
-    private fun FrameLayout.init(): Array<ImageView> {
-        val view = View.inflate(this@MorabarabaActivity, R.layout.mylayout, null)
-        this.addView(view)
-        return view.allViews.filterIsInstance<ImageView>().toList().toTypedArray()
     }
 
     private fun getSize() {
